ed9fd349730e004e2f7510005fa8267a2ad8be1b
Sown <ylies99@yahoo.com>
2024-02-25 09:22:38 +0100

    Ajout de la massadd / massremove

--- source/commands/Utilitaires/massremove.js
+++ source/commands/Utilitaires/massremove.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const Snoway = require('../../structures/client/index.js');
 
 module.exports = {
-    name: 'massadd',
+    name: 'massremove',
     description: {
-        fr: "Permet d'ajouter un rôle à la totalité du serveur",
-        en: "Adds a role to the entire server"
+        fr: "Permet de supprimer un rôle à la totalité du serveur",
+        en: "Removes a role from the entire server"
     },
     usage: {
-        fr: { "massadd <rôle>": "Permet d'ajouter un rôle à la totalité du serveur" },
-        en: { "massadd <rôle>": "Adds a role to the entire server" }
+        fr: { "massremove <rôle>": "Permet de supprimer un rôle à la totalité du serveur" },
+        en: { "massremove <rôle>": "Removes a role from the entire server" }
     },
     /**
      * 
@@ -26,24 +26,24 @@ module.exports = {
         }
 
         const members = message.guild.members.cache.filter(
-            (m) => !m.roles.cache.has(role.id)
+            (m) => m.roles.cache.has(role.id)
         );
 
         const totalMembers = members.size;
         const batchSize = Math.ceil(totalMembers * 0.05); 
         let count = 0;
 
-        message.channel.send(`[\`0%\`] Ajout du rôle : \`${role.name}\` à \`${totalMembers}\` membre${totalMembers !== 1 ? "s" : ""}`).then(async (msg) => {
+        message.channel.send(`[\`0%\`] Suppression du rôle : \`${role.name}\` à \`${totalMembers}\` membre${totalMembers !== 1 ? "s" : ""}`).then(async (msg) => {
             for (const [memberID, member] of members) {
-                await member.roles.add(role).catch(console.error); 
+                await member.roles.remove(role).catch(console.error); 
                 count++;
                 if (count % batchSize === 0 || count === totalMembers) {
                     const progress = Math.floor((count / totalMembers) * 100);
-                    msg.edit(`[\`${progress}%\`] Ajout du rôle : \`${role.name}\` à \`${totalMembers}\` membre${totalMembers !== 1 ? "s" : ""}`);
+                    msg.edit(`[\`${progress}%\`] Suppression du rôle : \`${role.name}\` à \`${totalMembers}\` membre${totalMembers !== 1 ? "s" : ""}`);
                 }
                 await new Promise(resolve => setTimeout(resolve, 800));
             }
-            msg.edit('Le rôle a été ajouté à tous les membres avec succès.');
+            msg.edit('Le rôle a été supprimé à tous les membres avec succès.');
         }).catch(console.error);
     }
 }
