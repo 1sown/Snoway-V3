300fbd28b58caea39cde42a1dea28b3b6e242e70
Sown <ylies99@yahoo.com>
2024-04-18 23:06:23 +0200

    Push

---
+++ .github/source/structures/Functions/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    private: "1188962621504888862",
+    snoway: {
+        panel: "",
+        api: ""
+    },
+    manager: {
+        panel: "",
+        key: ""
+    },
+    api: {
+        giphy: {
+            token: ""
+        }
+    }
+}
